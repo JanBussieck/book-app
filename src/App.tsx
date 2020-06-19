e80e6bf95140519a3c40d7da12f032f73f5d5e62
Jan Bussieck <jan.bussieck@gmail.com>
2020-06-19 13:37:53 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import {BookList} from './components/BookList';
 import {Container, Segment} from 'semantic-ui-react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <Segment raised>
-          <BookList />
-        </Segment>
-      </Container>
-    </div>
+    <Router>
+      <div className="App">
+        <Container>
+          <Segment raised>
+            <Link to="/books">Bücher</Link>
+            <Switch>
+              <Route path="/books">
+                <BookList />
+              </Route>
+            </Switch>
+          </Segment>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
