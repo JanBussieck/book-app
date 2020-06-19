174202811c5ce73e8a8ca1c3d670a57f15bccb30
Jan Bussieck <jan.bussieck@gmail.com>
2020-06-19 10:53:12 +0200

    Add book list to app

---
+++ src/components/BookList/BookItem.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Header, List} from 'semantic-ui-react'
+
+interface Book {
+  title: string,
+  subtitle: string,
+  isbn: string,
+  abstract: string,
+  numPages: number,
+  author: string,
+  publisher?: {
+    name: string,
+    url?: string
+  },
+  cover: string
+}
+
+export default function ({
+  title,
+  subtitle,
+  author
+}: Book) {
+  return (
+    <List.Item>
+      <List.Icon name='book' size='large' verticalAlign='middle' />
+      <List.Content>
+        <Header as='a'>{title}</Header>
+        <div>
+          {subtitle} von {author}
+        </div>
+      </List.Content>
+    </List.Item>
+  );
+};
